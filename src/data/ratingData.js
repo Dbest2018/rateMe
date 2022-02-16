f526c8e099a870b52ef93017427c6e8c50f5afdd
Shittu Ibrahim <shit2wice@gmail.com>
2022-02-16 00:31:59 +0000

    [feat] Add mock json rating data

---
+++ src/data/ratingData.js
@@ -0,0 +1,17 @@
+export const ratingData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Wow what an amazing product, It was really beneficial",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Great Product, would definitely recommend to my friends and family",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Nice product, would have given a perfect score but i had a few problems installing but it was due to bad network connection",
+  },
+];
