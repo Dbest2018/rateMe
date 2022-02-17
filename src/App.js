07c57213b0c2e39e3ea29ef15fccd20a1b3696d0
Shittu Ibrahim <shit2wice@gmail.com>
2022-02-17 00:56:40 +0000

    [feat] Add ratings stat

--- src/App.js
+++ src/App.js
@@ -8,22 +8,40 @@ import { RatingData } from "./data/RatingData";
 function App() {
   const [ratings, setRatings] = useState(RatingData);
 
+  let average =
+    ratings.reduce((acc, cur) => {
+      return acc + cur.score;
+    }, 0) / ratings.length;
+
+  average = average.toFixed(1).replace(/[.,]0$/, "");
+
   const deleteRating = (id) => {
     if (window.confirm("Are you sure you want to delete? ")) {
       setRatings(ratings.filter((rating) => rating.id !== id));
     }
   };
+
   return (
-    <div className="app">
+    <>
       <Header />
-      {ratings ? (
-        ratings.map((rating) => (
-          <Rating rating={rating} key={rating.id} handleDelete={deleteRating} />
-        ))
-      ) : (
-        <p>No ratings yet</p>
-      )}
-    </div>
+      <div className="container">
+        <div className="feedback-stats">
+          <h4>{ratings.length} Reviews</h4>
+          <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+        </div>
+        {ratings.length > 0 ? (
+          ratings.map((rating) => (
+            <Rating
+              rating={rating}
+              key={rating.id}
+              handleDelete={deleteRating}
+            />
+          ))
+        ) : (
+          <p>No ratings yet</p>
+        )}
+      </div>
+    </>
   );
 }
 
